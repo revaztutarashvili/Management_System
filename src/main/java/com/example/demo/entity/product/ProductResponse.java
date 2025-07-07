3b3a1b393114b8bef548ec4ad5632254d8f84018
revaztutarashvili <generationofjeans@gmail.com>
2025-07-08 00:08:18 +0400

    add model full

--- src/main/java/com/example/demo/entity/product/ProductResponse.java
+++ src/main/java/com/example/demo/entity/product/ProductResponse.java
@@ -1,4 +1,43 @@
 package com.example.demo.entity.product;
 
 public class ProductResponse {
+
+    private Integer count;
+
+    private String name;
+
+    private String description;
+
+    public ProductResponse() {
+    }
+
+    public ProductResponse(Integer count, String name, String description) {
+        this.count = count;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/example/demo/entity/user/UserResponse.java
+++ src/main/java/com/example/demo/entity/user/UserResponse.java
@@ -1,4 +1,32 @@
 package com.example.demo.entity.user;
 
 public class UserResponse {
+
+    public String userName;
+
+    public String name;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(String userName, String name) {
+        this.userName = userName;
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
